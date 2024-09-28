1797f8c8b56bb2f3cf3f54e3941b5fab5d4b4ff2
OzlemKaraboga <ozlemkaraboga33@gmail.com>
2024-09-28 18:17:52 +0200

    Updated

--- movie/src/pages/HomePage.jsx
+++ movie/src/pages/HomePage.jsx
@@ -28,11 +28,9 @@ export const HomePage = () => {
     const apiUrl = API_URL;
     const apiKey = API_KEY;
     const [movies, setMovies] = useState([]);
-    const title =
-        defaultCategories[Math.floor(Math.random() * defaultCategories.length)];
-    const currentYear = new Date().getFullYear();
+    const title = defaultCategories[Math.floor(Math.random() * defaultCategories.length)];
     const [isDataLoading, error, getJson] = useFetch(
-        `${apiUrl}?apikey=${apiKey}&s=${title}&y=${currentYear}`,
+        `${apiUrl}?apikey=${apiKey}&s=${title}&y=${''}`,
         setMovies,
     );
     useEffect(() => {
